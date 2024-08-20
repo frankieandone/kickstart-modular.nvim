46d135b0f7abc493caf176a35129c56a8c576cd5
Frankie Merzadyan <frankie@mindhuntr.ai>
2024-08-20 03:25:02 +0100

    New: colourise hex (and others) strings

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -10,18 +10,7 @@
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-    -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
     'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
-    -- NOTE: Plugins can also be added by using a table,
-    -- with the first argument being the link and the following
-    -- keys can be used to configure plugin behavior/loading/etc.
-    --
-    -- Use `opts = {}` to force a plugin to be loaded.
-    --
-
-    -- modular approach: using `require 'path/name'` will
-    -- include a plugin definition from file lua/path/name.lua
 
     require 'plugins.nvim-web-devicons',
     require 'plugins.nvim-tree',
@@ -32,42 +21,23 @@ require('lazy').setup({
     require 'plugins.lspconfig',
     require 'plugins.conform',
     require 'plugins.cmp',
-
-    require 'plugins.which-key',
-
-    require 'plugins.todo-comments',
+    require 'plugins.trouble',
+    require 'plugins.null-ls',
+    require 'plugins.lint',
 
     require 'plugins.mini',
 
-    -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-    -- init.lua. If you want these files, they are in the repository, so you can just download them and
-    -- place them in the correct locations.
-
-    -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-    --
-    --  Here are some example plugins that I've included in the Kickstart repository.
-    --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-    --
-    require 'plugins.debug',
-    require 'plugins.indent_line',
-    require 'plugins.lint',
     require 'plugins.autopairs',
-    require 'plugins.null-ls',
+    require 'plugins.indent_line',
     require 'plugins.grug-far',
-    require 'plugins.trouble',
-    -- require 'plugins.neo-tree', --use nvim-tree instead
+    require 'plugins.todo-comments',
+    require 'plugins.nvim-colorizer',
 
-    -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-    --    This is the easiest way to modularize your config.
-    --
-    --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-    --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-    -- { import = 'custom.plugins' },
+    require 'plugins.which-key',
+    require 'plugins.debug',
 }, {
     ui = {
-        -- If you are using a Nerd Font: set icons to an empty table which will use the
-        -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-        icons = vim.g.have_nerd_font and {} or {},
+        icons = vim.g.have_nerd_font or {},
     },
 })
 
